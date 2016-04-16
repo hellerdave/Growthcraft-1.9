db8be2fdd038310bacc147b52b34c16f71eb0f30
Jamie Quinn <jjquinn42@gmail.com>
2016-04-16 16:56:52 +0100

    fishtrap: Fishtrap block added; no functionality

--- src/java/growthcraft/fishtrap/GrowthCraftFishTrap.java
+++ src/java/growthcraft/fishtrap/GrowthCraftFishTrap.java
@@ -1,11 +1,17 @@
 package growthcraft.fishtrap;
 
+import growthcraft.core.common.definition.BlockDefinition;
+import growthcraft.fishtrap.common.block.BlockFishTrap;
+
+import net.minecraftforge.fml.common.registry.GameRegistry;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.Mod.Instance;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.oredict.ShapedOreRecipe;
+import net.minecraft.init.Items;
 
 @Mod(
 	modid = GrowthCraftFishTrap.MOD_ID,
@@ -21,15 +27,31 @@ public class GrowthCraftFishTrap
 
 	@Instance(MOD_ID)
 	public static GrowthCraftFishTrap instance;
+	
+	public static BlockDefinition fishTrap;
 
 	@EventHandler
 	public void preload(FMLPreInitializationEvent event)
 	{
+		fishTrap = new BlockDefinition(new BlockFishTrap());
+
+		registerBlocks();
+	}
+
+	private void registerBlocks()
+	{
+		GameRegistry.registerBlock(fishTrap.getBlock(), "grc.fishTrap");
+	}
+
+	private void registerRecipes()
+	{
+		GameRegistry.addRecipe(new ShapedOreRecipe(fishTrap.asStack(1), "ACA", "CBC", "ACA", 'A', "plankWood", 'B', Items.lead, 'C', Items.string));
 	}
 
 	@EventHandler
 	public void load(FMLInitializationEvent event)
 	{
+		registerRecipes();
 	}
 
 	@EventHandler
---
+++ src/java/growthcraft/fishtrap/common/block/BlockFishTrap.java
@@ -0,0 +1,19 @@
+package growthcraft.fishtrap.common.block;
+
+import growthcraft.core.GrowthCraftCore;
+
+import net.minecraft.block.material.Material;
+import net.minecraft.block.Block;
+
+public class BlockFishTrap extends Block
+{
+	public BlockFishTrap()
+	{
+		super(Material.wood);
+		setTickRandomly(true);
+		setHardness(0.4F);
+		setStepSound(soundTypeWood);
+		setUnlocalizedName("grc.fishTrap");
+		setCreativeTab(GrowthCraftCore.creativeTab);
+	}
+}
