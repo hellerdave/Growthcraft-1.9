c8a4d8efb545d9692c5ffa59ffbfe02c6a1ef0d1
Jamie Quinn <jjquinn42@gmail.com>
2016-04-18 12:33:44 +0100

    fishtrap: fishtrap block now renders with textures

--- src/java/growthcraft/fishtrap/client/ClientProxy.java
+++ src/java/growthcraft/fishtrap/client/ClientProxy.java
@@ -1,17 +1,22 @@
 package growthcraft.fishtrap.client;
 
 import growthcraft.fishtrap.common.CommonProxy;
+import growthcraft.core.util.ModelResourceLocationFactory;
 
+import net.minecraftforge.client.model.ModelLoader;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
 public class ClientProxy extends CommonProxy 
 {
+	public final ModelResourceLocationFactory modelResLoc = new ModelResourceLocationFactory("growthcraft-fishtrap");
+
 	@Override
 	public void preInit(FMLPreInitializationEvent event) 
 	{
 		super.preInit(event);
+		ModelLoader.setCustomModelResourceLocation(blocks.fishTrap.getItem(), 0, modelResLoc.create(blocks.fishTrap.getBlock().getUnlocalizedName().substring(5), "inventory"));
 	}
 
 	@Override
